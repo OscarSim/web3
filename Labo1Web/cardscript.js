6c9b9d498431882366812f8116ffed11e468e916
Oscar Simon <2130484@etu.cegepjonquiere.ca>
2023-03-06 17:12:48 -0500

    Web lab2 a moitier fait (fini avant la fin de la semaine)

---
+++ Labo1Web/cardscript.js
@@ -0,0 +1,14 @@
+fetch("http://localhost:3000/publication/")
+.then(data => data.json())
+.then(json =>{
+    json.forEach(publication => {
+        let carte = '<div class="card me-5 mb-5" style="width:500px">' +
+        '<img class="card-img-top" src="'+publication["image"]+'" alt="Card image" style="width:100%">' +
+        '<div class="card-body">' +
+        '<h4 class="card-title border border-dark px-2 py-2">'+publication["titre"]+'</h4>' +
+        '<p class="card-text">'+ publication["contenu"]+'</p>' +
+        '</div>'+
+        '</div>'
+    $("#publicationAuto").append($(carte));    
+    });
+})
